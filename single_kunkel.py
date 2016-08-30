44f639d4d4ce6883651fd6efbb9f056f724c7362
Alex Carlin <carlin@ucdavis.edu>
2016-08-30 14:00:56 -0700

    Still working on single kunkel

--- single_kunkel.py
+++ single_kunkel.py
@@ -12,10 +12,10 @@ def single_kunkel( p, params ):
     #   ssDNA               Transcriptic ID of ssDNA to use
 
     # df = pandas.read_csv( params[ 'order_csv' ] )
-    df = pandas.read_csv( 'example.csv', index_col=0 )
+    df = pandas.read_csv( 'example2.csv' )
 
-    df = pandas.concat( [ df, df, df ] ) # makes 96 mutants for debugging
-    df.index = range( len( df ) )        # for debugging only!
+    #df = pandas.concat( [ df, df, df ] ) # makes 96 mutants for debugging
+    #df.index = range( len( df ) )        # for debugging only!
 
     # order the oligos
     oligo_pt = p.ref( 'oligo_pt', None, '96-pcr', storage='cold_20' )
@@ -29,7 +29,6 @@ def single_kunkel( p, params ):
     kin_mix = provision_helper( p, kinase_mix, len( df ) )
     kin_pt = p.ref( 'kin_pt', None, '96-pcr', discard=True )
     p.transfer( kin_mix.well( 0 ), kin_pt.wells( ML ), '23:microliter' )
-    #p.transfer( oligo_pt.wells( ML ), kin_pt.wells( ML ), '7:microliter' )
     p.stamp( oligo_pt, kin_pt, '7:microliter' )
     p.seal( kin_pt )
     p.incubate( kin_pt, 'warm_37', '60:minute' )
@@ -44,7 +43,7 @@ def single_kunkel( p, params ):
     p.uncover( dil_pt )
 
     # anneal
-    my_ssdna = p.ref( 'my_ssdna', 'ct18bf7kgp82fs', 'micro-1.5', discard=True )
+    my_ssdna = p.ref( 'my_ssdna', 'ct18bf7kgp82fs', 'micro-1.5', storage='cold_20' )
     mix = p.ref( 'mix', None, 'micro-1.5', storage='cold_20' )
     anneal_pt = p.ref( 'anneal_pt', None, '384-pcr', storage='cold_20' )
     p.transfer( mix.well(0), anneal_pt.wells( ML ), '2:microliter' )
@@ -59,36 +58,52 @@ def single_kunkel( p, params ):
     p.seal( anneal_pt )
     p.incubate( anneal_pt, 'ambient', '90:minute' )
 
-    # transform
+    # transform, recover, and plate
     p.incubate( anneal_pt, 'cold_4', '20:minute' )
+    p.unseal( anneal_pt )
     p.provision( 'rs16pbj944fnny', anneal_pt.wells( ML ), '10:microliter' )
-    p.cover( anneal_pt )
+    p.seal( anneal_pt )
     p.incubate( anneal_pt, 'cold_4', '20:minute' )
     grow_pt = p.ref( 'grow_pt', None, '96-deep', discard=True )
     p.dispense_full_plate( grow_pt, 'tb_50ug_ml_kan', '100:microliter' )
+    p.unseal( anneal_pt )
     p.stamp( anneal_pt, grow_pt, '15:microliter' )
     p.cover( grow_pt )
-    p.incubate( grow_pt, 'warm_37', '10:hour', shaking=True )
+    p.incubate( grow_pt, 'warm_37', '90:minute', shaking=True )
 
-    agar_pt_names = []
-    kit_item = Container( None, p.container_type( '6-flat' ) )
-    for i in range( len(df)/6 ):
-        name = 'agar_{}'.format( i )
-        my_ref = Ref( name, { 'reserve': 'ki17rs7j799zc2', 'store': { 'where': 'cold_4' } }, kit_item )
-        p.refs[ name ] = my_ref
-        agar_pt_names.append( name )
-    for i in range( len( df ) ):
-        p.spread( grow_pt.well( i ), p.refs[ 'agar_{}'.format( i/6 ) ].container.well( i%6 ), '55:microliter' )
+    for i in ML: # Kunkel indexes
+        print i
 
-    agar_pts = [ p.refs[ n ] for n in agar_pt_names ]
-    for agar_pt in agar_pts:
-        #print agar_pt.name
-        #print dir( agar_pt )
-        p.cover( agar_pt )
-        p.incubate( agar_pt, 'warm_37', '10:hour' )
-        p.uncover( agar_pt )
-        p.image_plate( agar_pt, mode='top', dataref='jeff' )
-        p.cover( agar_pt )
+    #
+    # class ContainerGroup:
+    #     def __init__( self, n_cont, cont_type, storage ):
+    #         self.container_group = []
+    #         for i in range( n_cont ):
+    #             my_ref = p.ref( 'cont_{}'.format( i ), None, cont_type, storage )
+    #             self.container_group.append( my_ref )
+    #
+    #
+    #
+    #
+    # #agar_pt_names = []
+    # kit_item = Container( None, p.container_type( '6-flat' ) )
+    # for i in range( len(df)/6 ):
+    #     name = 'agar_{}'.format( i )
+    #     my_ref = Ref( name, { 'reserve': 'ki17rs7j799zc2', 'store': { 'where': 'cold_4' } }, kit_item )
+    #     #p.refs[ name ] = my_ref
+    #     #agar_pt_names.append( name )
+    # for i in range( len( df ) ):
+    #     p.spread( grow_pt.well( i ), p.refs[ 'agar_{}'.format( i/6 ) ].container.well( i%6 ), '55:microliter' )
+
+    # agar_pts = [ p.refs[ n ] for n in agar_pt_names ]
+    # for agar_pt in agar_pts:
+    #     #print agar_pt.name
+    #     #print dir( agar_pt )
+    #     p.cover( agar_pt )
+    #     p.incubate( agar_pt, 'warm_37', '10:hour' )
+    #     p.uncover( agar_pt )
+    #     p.image_plate( agar_pt, mode='top', dataref='jeff' )
+    #     p.cover( agar_pt )
 
 if __name__ == '__main__':
     from autoprotocol.harness import run
