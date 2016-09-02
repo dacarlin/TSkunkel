309d5729b5be3800c58c3f5c606f10c0eada7ef0
Alex Carlin <carlin@ucdavis.edu>
2016-09-02 11:00:32 -0700

    Draft of single Kunkel running through transformation

--- kunkel_recipies.py
+++ kunkel_recipies.py
@@ -7,12 +7,26 @@ kinase_mix = [
     ( 'water', 18, 'rs17gmh5wafm5p' ),
 ]
 
+kinase_mix_resus = [
+    ( 'pnk_buffer', 1, 'rs16pc9rd5sg5d' ),
+    ( 'atp', 0.3, 'rs16pccshb6cb4' ),
+    ( 'pnk', 0.3, 'rs16pc9rd5hsf6' ),
+    ( 'water', 8.4, 'rs17gmh5wafm5p' ),
+]
+
 pol_mix = [
     ( 'lig_buffer', 0.6, 'rs17sh5rzz79ct' ),
     ( 'dntps', 0.4, 'rs16pcb542c5rd' ),
     ( 'atp', 0.4, 'rs16pccshb6cb4' ),
     ( 'pol', 0.4, 'rs16pca2urcz74' ),
     ( 'ligase', 0.4, 'rs16pc8krr6ag7' ),
+]
+
+nick_mix = [
+    ( 'nt', 1, 'rs18x6k25qmr6k' ), # actually regular BbvCI, warning!
+    ( 'exo', 1, 'rs192ke3hj5d3d' ), # ExoI (E. coli)
+    ( 'cutsmart', 1, 'rs17ta93g3y85t' ), # 10X CutSmart
+    ( 'water', 7, 'rs17gmh5wafm5p' ),
 ]
 
 def provision_helper( p, recipe, n ):
--- single_kunkel.py
+++ single_kunkel.py
@@ -1,109 +1,110 @@
-from kunkel_recipies import kinase_mix, pol_mix, provision_helper, ramp
+from kunkel_recipies import kinase_mix_resus, nick_mix, pol_mix, provision_helper, ramp
 from autoprotocol.container import Container
 from autoprotocol.protocol import Ref
 import pandas
 
 def single_kunkel( p, params ):
 
+    # before running this protocol
+    # transform your plasmid into CJ236
+    # grow a large culture (1 L)
+    # purify plasmid DNA from the culture
+
+    dsdna = True # set to False if you have already prepped ssDNA
+
     # format of input CSV of order:
     #
     #   mutant_label        arbitrary string
     #   sequence            sequence of mutageneic oligo
-    #   ssDNA               Transcriptic ID of ssDNA to use
 
     # df = pandas.read_csv( params[ 'order_csv' ] )
     df = pandas.read_csv( 'example2.csv' )
+    df = pandas.concat( [ df, df, df ] ) # makes 96 mutants for debugging
+    df.index = range( len( df ) )
 
-    #df = pandas.concat( [ df, df, df ] ) # makes 96 mutants for debugging
-    #df.index = range( len( df ) )        # for debugging only!
+    ML = list( df.index ) # we will need this list of int a lot :)
+    n = len( ML )
 
-    # order the oligos
+    # order the oligos into a plate, they come dry, resuspend in kinase mix
     oligo_pt = p.ref( 'oligo_pt', None, '96-pcr', storage='cold_20' )
     p.oligosynthesize( [ dict( destination='oligo_pt/{}'.format(idx), sequence=x.sequence, scale='10nm' ) for idx, x in df.iterrows() ] )
-    # oligos come normalized, wet in 96-well plate
-    # see http://www.idtdna.com/pages/products/dna-rna/96-and-384-well-plates
-    # may need p.unseal( oligo_pt )
 
     # kinase
-    ML = list( df.index ) # we will need this list of int a lot :)
-    kin_mix = provision_helper( p, kinase_mix, len( df ) )
-    kin_pt = p.ref( 'kin_pt', None, '96-pcr', discard=True )
-    p.transfer( kin_mix.well( 0 ), kin_pt.wells( ML ), '23:microliter' )
-    p.stamp( oligo_pt, kin_pt, '7:microliter' )
-    p.seal( kin_pt )
-    p.incubate( kin_pt, 'warm_37', '60:minute' )
-    p.unseal( kin_pt )
+    kin_mix = p.ref( 'kin_mix', None, 'micro-2.0', discard=True )
+    for reagent, vol, my_id in kinase_mix_resus:
+        p.provision( my_id, kin_mix.well(0), '{}:microliter'.format( vol * ( n + 2 ) ) )
+    p.transfer( kin_mix.well( 0 ), oligo_pt.wells( ML ), '10:microliter', mix_after=True )
+    p.seal( oligo_pt )
+    p.spin( oligo_pt, '2000:g', '30:second' )
+    p.incubate( oligo_pt, 'warm_37', '60:minute' )
+    # may have to remove some liquid here to bring down to  1 uL
+    p.unseal( oligo_pt )
 
     # dilute
-    dil_pt = p.ref( 'dil_pt', None, '96-flat', discard=True )
-    p.dispense_full_plate( dil_pt, 'water', '200:microliter' )
-    p.stamp( kin_pt, dil_pt, '2:microliter' )
-    p.cover( dil_pt )
-    p.spin( dil_pt, '2000:g', '30:second' )
-    p.uncover( dil_pt )
+    p.dispense_full_plate( oligo_pt, 'water', '95:microliter' )
+
+    # reagents and ssDNA master mix for annealing
+    nk_mix = p.ref( 'nick_mix', None, 'micro-2.0', discard=True )
+    if dsdna:
+        for reagent, vol, my_id in nick_mix:
+            p.provision( my_id, nk_mix.well( 0 ), '{}:microliter'.format( vol * ( n + 1 ) ) )
+    else:
+        p.provision( 'rs17sh5rzz79ct', nk_mix.well( 0 ), '{}:microliter'.format( .2 * ( n + 1 ) ) )
+    my_ssdna = p.ref( 'my_ssdna', 'ct18bf7kgp82fs', 'micro-1.5', storage='cold_20' )
+    p.transfer( my_ssdna.well( 0 ), nk_mix.well( 0 ), '{}:microliter'.format( 2 * ( n + 1 ) ) )
+    if dsdna:
+        p.incubate( nk_mix, 'warm_37', '1:hour' )
 
     # anneal
-    my_ssdna = p.ref( 'my_ssdna', 'ct18bf7kgp82fs', 'micro-1.5', storage='cold_20' )
-    mix = p.ref( 'mix', None, 'micro-1.5', storage='cold_20' )
     anneal_pt = p.ref( 'anneal_pt', None, '384-pcr', storage='cold_20' )
-    p.transfer( mix.well(0), anneal_pt.wells( ML ), '2:microliter' )
-    p.transfer( my_ssdna.well(0), anneal_pt.wells( ML ), '2.2:microliter' )
+    p.distribute( nk_mix.well( 0 ), anneal_pt.wells( ML ), '2:microliter' )
+    p.stamp( oligo_pt, anneal_pt.well(0), '2:microliter' )
     p.seal( anneal_pt )
     p.thermocycle( anneal_pt, [ { "cycles": 1, "steps": ramp } ] )
 
     # polymerize
-    pmix = provision_helper( p, pol_mix, len( df ) )
+    pmix = provision_helper( p, pol_mix, n )
     p.unseal( anneal_pt )
-    p.transfer( pmix.well(0), anneal_pt.wells_from( 0, len(df) ), '2:microliter', one_tip=False )
+    p.transfer( pmix.well(0), anneal_pt.wells( ML ), '2:microliter' )
     p.seal( anneal_pt )
     p.incubate( anneal_pt, 'ambient', '90:minute' )
 
     # transform, recover, and plate
-    p.incubate( anneal_pt, 'cold_4', '20:minute' )
+    p.incubate( anneal_pt, 'cold_4', '10:minute' )
     p.unseal( anneal_pt )
     p.provision( 'rs16pbj944fnny', anneal_pt.wells( ML ), '10:microliter' )
     p.seal( anneal_pt )
-    p.incubate( anneal_pt, 'cold_4', '20:minute' )
+    p.incubate( anneal_pt, 'cold_4', '10:minute' )
     grow_pt = p.ref( 'grow_pt', None, '96-deep', discard=True )
-    p.dispense_full_plate( grow_pt, 'tb_50ug_ml_kan', '100:microliter' )
+    p.dispense_full_plate( grow_pt, 'soc', '100:microliter' )
     p.unseal( anneal_pt )
-    p.stamp( anneal_pt, grow_pt, '15:microliter' )
+    p.stamp( anneal_pt.well(0), grow_pt, '15:microliter' )
     p.cover( grow_pt )
     p.incubate( grow_pt, 'warm_37', '90:minute', shaking=True )
 
-    for i in ML: # Kunkel indexes
-        print i
+    # some trickery still around agar plates at Transcriptic
+    my_plates = []
+    for i in list( set( map( lambda x: x/6, ML ) ) ): # used to get indexes for the 6-well plates
+        name = 'agar_{}'.format( i )
+        my_ref = Ref( name, { 'reserve': 'ki17rs7j799zc2', 'store': { 'where': 'cold_4' } }, Container( None, p.container_type( '6-flat' ) ) )
+        p.refs[ name ] = my_ref
+        my_plates.append( my_ref.container )
 
-    #
-    # class ContainerGroup:
-    #     def __init__( self, n_cont, cont_type, storage ):
-    #         self.container_group = []
-    #         for i in range( n_cont ):
-    #             my_ref = p.ref( 'cont_{}'.format( i ), None, cont_type, storage )
-    #             self.container_group.append( my_ref )
-    #
-    #
-    #
-    #
-    # #agar_pt_names = []
-    # kit_item = Container( None, p.container_type( '6-flat' ) )
-    # for i in range( len(df)/6 ):
-    #     name = 'agar_{}'.format( i )
-    #     my_ref = Ref( name, { 'reserve': 'ki17rs7j799zc2', 'store': { 'where': 'cold_4' } }, kit_item )
-    #     #p.refs[ name ] = my_ref
-    #     #agar_pt_names.append( name )
-    # for i in range( len( df ) ):
-    #     p.spread( grow_pt.well( i ), p.refs[ 'agar_{}'.format( i/6 ) ].container.well( i%6 ), '55:microliter' )
+    # sample-centric spreading (can't think of a simple way to do it another way)
+    for m in ML:
+        p.spread( grow_pt.well( m ), my_plates[ m / 6 ].well( m % 6 ), '55:microliter' )
 
-    # agar_pts = [ p.refs[ n ] for n in agar_pt_names ]
-    # for agar_pt in agar_pts:
-    #     #print agar_pt.name
-    #     #print dir( agar_pt )
-    #     p.cover( agar_pt )
-    #     p.incubate( agar_pt, 'warm_37', '10:hour' )
-    #     p.uncover( agar_pt )
-    #     p.image_plate( agar_pt, mode='top', dataref='jeff' )
-    #     p.cover( agar_pt )
+    # incubate and image
+    for agar_pt in my_plates:
+        p.incubate( agar_pt, 'warm_37', '10:hour' )
+
+    for idx, agar_pt in enumerate( my_plates ):
+        #p.uncover( agar_pt )
+        p.image_plate( agar_pt, mode='top', dataref='agar_{}'.format( idx ) )
+        #p.cover( agar_pt )
+
+    #grow_pt2 = p.ref( 'grow_pt2', None, '96-deep', discard=True )
+
 
 if __name__ == '__main__':
     from autoprotocol.harness import run
